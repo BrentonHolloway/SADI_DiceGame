381130bc497b3d803a26a765e65615001f55ff36
BrentonHolloway <brenton.holloway@gmail.com>
2018-03-01 18:52:57 +1100

    update 01-03-18

--- src/model/SimplePlayer.java
+++ src/model/SimplePlayer.java
@@ -4,62 +4,70 @@ import model.interfaces.DicePair;
 import model.interfaces.Player;
 
 public class SimplePlayer implements Player{
-	public SimplePlayer(String playerNum, String name, int credit) {
-		
+	
+	String playerNum;
+	String name;
+	int points;
+	int bet;
+	DicePair roll;
+	
+	public SimplePlayer(String playerNum, String name, int points) {
+		this.playerNum = playerNum;
+		this.name = name;
+		this.points = points;
 	}
 
 	@Override
 	public String getPlayerName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
 
 	@Override
 	public void setPlayerName(String playerName) {
-		// TODO Auto-generated method stub
+		this.name = playerName;
 		
 	}
 
 	@Override
 	public int getPoints() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.points;
 	}
 
 	@Override
 	public void setPoints(int points) {
-		// TODO Auto-generated method stub
+		this.points = points;
 		
 	}
 
 	@Override
 	public String getPlayerId() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.playerNum;
 	}
 
 	@Override
 	public boolean placeBet(int bet) {
-		// TODO Auto-generated method stub
-		return false;
+		if(bet <= points) {
+			this.points -= bet;
+			this.bet += bet;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public int getBet() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.bet;
 	}
 
 	@Override
 	public DicePair getRollResult() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.roll;
 	}
 
 	@Override
 	public void setRollResult(DicePair rollResult) {
-		// TODO Auto-generated method stub
-		
+		this.roll = rollResult;
 	}
 
 }
